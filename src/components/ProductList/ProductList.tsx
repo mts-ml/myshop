6ecf5c9dea0c5752681f8a22d6c02fed9ae21e90
Mateus <mtsmlima@gmail.com>
2025-08-28 16:00:21 -0300

    refactor(product-list): remove unused comments and organize imports

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 import { ProductCard } from '../ProductCard/ProductCard'
 import { Product } from '../../data/products'
-import axios from 'axios'
 
 import * as S from './productListStyles'
 
@@ -9,13 +10,12 @@ import * as S from './productListStyles'
 export const ProductList: React.FC = () => {
    const [products, setProducts] = useState<Product[]>([])
 
-
-    // Buscar dados da API
    useEffect(() => {
       async function fetchProducts() {
          try {
             const response = await axios.get('https://fakestoreapi.com/products')
             setProducts(response.data)
+
          } catch (error) {
             throw new Error(`Falha ao buscar dados na API: ${error}`)
          }
